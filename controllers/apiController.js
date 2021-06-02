47b3c0a29b99c8687fd5864588cd8c21397db9fb
Zarzyk <sebekzarzyk@gmail.com>
2021-06-02 15:56:54 +0200

    Added CRUD for user, result

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -32,9 +32,79 @@ const updateText = async (req,res) =>{
     res.send("Text "+id +" updated successfully")
 }
 
+const addUser = async (req,res) =>{
+    const email = req.body.email;
+    const password = req.body.password;
+    const response = await pool.query('INSERT INTO account (email,password) values ($1,$2)',[email,password]);
+    res.status(200).json(response);
+}
+
+const getUsers = async (req, res) => {
+    const response = await pool.query('SELECT * FROM account');
+    res.status(200).json(response.rows)
+}
+
+const getUserById = async (req,res) =>{
+    const id = req.params.id;
+    const response = await pool.query('SELECT * FROM account WHERE id=$1',[id]);
+    res.status(200).json(response.rows)
+}
+
+const updateUser = async (req,res) =>{
+    const id = req.params.id;
+    const email = req.body.email;
+    const password = req.body.password;
+    pool.query('UPDATE account SET email = $1,password = $2 WHERE id = $3',[email,password,id])
+    res.send("User "+id +" updated successfully")
+}
+
+const addResult = async (req,res) =>{
+    const time = req.body.time;
+    const wpm = req.body.wpm;
+    const accuracy = req.body.accuracy;
+    const text_id = req.body.text_id;
+    const user_id = req.body.user_id;
+    const response = await pool.query('INSERT INTO result (time,wpm,accuracy,text_id,user_id) values ($1,$2,$3,$4,$5)',[time,wpm,accuracy,text_id,user_id]);
+    res.status(200).json(response);
+}
+
+const getUserResultById = async (req,res) =>{
+    const userId = req.params.userId;
+    const resultId = req.params.resultId;
+    const response = await pool.query('SELECT * FROM result WHERE id=$1 and user_id=$2',[resultId,userId]);
+    res.status(200).json(response.rows)
+}
+
+const getUserResults = async (req,res) =>{
+    const id = req.params.id;
+    const response = await pool.query('SELECT * FROM result WHERE user_id=$1',[id]);
+    res.status(200).json(response.rows)
+}
+
+const updateResult = async (req,res) =>{
+    const id = req.params.id;
+    const time = req.body.time;
+    const wpm = req.body.wpm;
+    const accuracy = req.body.accuracy;
+
+    pool.query('UPDATE result SET time = $1,wpm = $2,accuracy = $3 WHERE id = $4',[time,wpm,accuracy,id])
+    res.send("Result "+id +" updated successfully")
+}
+
+
 module.exports={
     getTexts,
     addText,
     getTextById,
-    updateText
+    updateText,
+
+    addUser,
+    getUsers,
+    getUserById,
+    updateUser,
+
+    addResult,
+    getUserResults,
+    getUserResultById,
+    updateResult
 }
--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,21 @@
 const {Router} = require('express');
 const router = Router();
 
-const { getTexts,addText,getTextById,updateText } = require('../controllers/apiController')
+const { getTexts,addText,getTextById,updateText,addUser,updateUser,getUserById,getUsers,addResult,getUserResults,getUserResultById,updateResult } = require('../controllers/apiController')
 
 router.get('/texts', getTexts);
 router.get('/texts/:id',getTextById);
 router.post('/texts',addText);
 router.put('/texts/:id',updateText);
+
+router.get('/users', getUsers);
+router.get('/users/:id',getUserById);
+router.post('/users',addUser);
+router.put('/users/:id',updateUser);
+
+router.post('/users/:id/results',addResult)
+router.get('/users/:id/results',getUserResults)
+router.get('/users/:userId/results/:resultId',getUserResultById);
+router.put('/users/:id/results',updateResult)
 
 module.exports = router;
